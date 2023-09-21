085adfce09f0eaa26ec35ee284a8be6d35068e37
Đorđe Bojković <15720022+webhostingsrbijaRS@users.noreply.github.com>
2023-09-21 12:10:44 +0200

    Create head-and-foot.php

---
+++ head-and-foot.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       Head & Foot
+ * Plugin URI:        https://github.com/webhostingsrbijaRS/head-and-foot/
+ * Description:       Allows users to insert custom scripts in the header and footer.
+ * Version:           1.0
+ * Requires at least: 6.2
+ * Requires PHP:      8.0
+ * Author:            WebHostingSrbija
+ * Author URI:        https://www.webhostingsrbija.rs/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/webhostingsrbijaRS/head-and-foot/
+ * Domain Path:       /languages/
+ * Text Domain:       head-and-foot
+ */
